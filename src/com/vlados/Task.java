6cc19e10acfd119c3624200fad82bc50a608b400
vladachugay <chugay.vlada@gmail.com>
2020-09-29 21:23:36 +0300

    Craeted project for labs. Got started working at first lab.

---
+++ src/com/vlados/Task.java
@@ -0,0 +1,16 @@
+package com.vlados;
+
+import java.util.Scanner;
+
+public class Task {
+
+    private String getUsersLine() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine().trim();
+    }
+
+    private String[][] getMatrixFromUser() {
+        
+        String[][] matrix = new String[][];
+    }
+}
